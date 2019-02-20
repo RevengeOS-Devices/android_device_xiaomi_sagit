d460e98130dd50c05866f6bfc2621b5e383d620b
verevka <verevka86@gmail.com>
2019-02-20 23:07:01 +0300

    sagit: Welcome RevengeOS

--- revengeos_sagit.mk
+++ revengeos_sagit.mk
@@ -1,7 +1,15 @@
 $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
-$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-PRODUCT_NAME := nitrogen_sagit
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/gsm.mk)
+
+# Inherit some common AOSP stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+
+PRODUCT_NAME := revengeos_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
@@ -13,9 +21,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
-
-# Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-
-PRODUCT_PACKAGES += \
-    messaging
